eee27ac3d03afed46537d2d728f9b2aec26051a5
guilherme-menezes <guilherme.montnezes@gmail.com>
2023-03-24 21:40:42 -0300

    First commit

---
+++ src/main/kotlin/com/guiodes/multifunctionalcalculator/MultifunctionalCalculatorApplication.kt
@@ -0,0 +1,11 @@
+package com.guiodes.multifunctionalcalculator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultifunctionalCalculatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<MultifunctionalCalculatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/guiodes/multifunctionalcalculator/MultifunctionalCalculatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.guiodes.multifunctionalcalculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultifunctionalCalculatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
