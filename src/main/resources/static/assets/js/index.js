a373d34c0d85492f9368b6ff190bcdc3686803fd
guilherme-menezes <guilherme.montnezes@gmail.com>
2023-05-29 19:16:14 -0300

    Integration

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -1,8 +1,46 @@
 import { API_URL, POST_METHOD } from './constants.js';
-import { bhaskaraValidation, pitagorasValidation } from './validators.js';
+import { bhaskaraValidation, interestValidation, pitagorasValidation } from './validators.js';
 
 $(document).ready(function() {
     $("#navbar").load("components/navbar.html");
+
+    $('.form-simple-interest').submit(function(e) {
+        $('#div-resolution').html('');
+        $('#div-response').html('');
+
+        e.preventDefault();
+
+        let form = $(this);
+      
+        var formData = form.serializeArray().map((it) => it['value']);
+
+        let isSimpleInterest = formData[5];
+
+        const request = {
+            interest: formData[0],
+            amount: formData[1],
+            rate: formData[2],
+            capital: formData[3],
+            duration: formData[4],
+        };
+
+        interestValidation(request);
+
+        let interestType = isSimpleInterest ? "simple" : "compound";
+
+        sendRequest(POST_METHOD, `${API_URL}/${interestType}`, JSON.stringify(request), (data) => {
+            let resolution = data['resolution'];
+            let response = data['result'];
+
+            resolution.forEach(element => {
+                $('#div-resolution').append(`${element}<br>`);
+            });
+
+            console.log(response);
+
+            $('#div-response').append(`Resultado final: ${response}`);
+        })
+    });
 
     $('.form-3-fields').submit(function(e) {
         $('#div-resolution').html('');
--- src/main/resources/static/assets/js/validators.js
+++ src/main/resources/static/assets/js/validators.js
@@ -16,7 +16,7 @@ export function bhaskaraValidation(request) {
 }
 
 export function pitagorasValidation(request) {
-    let emptyFields = 0;
+    let emptyFields = 0;    
 
     if (isNaN(request['a']) || isNaN(request['b']) || isNaN(request['c'])) {
         throw Error('Valor inválido para pelo menos um dos campos')
@@ -37,5 +37,38 @@ export function pitagorasValidation(request) {
     if (emptyFields > 1) {
         $('#div-response').html('Você deve preencher 2 dos 3 campos disponíveis para realizar a operação!')
         throw Error('O usuário deve preencher 2 dos 3 campos disponíveis para realizar a operação!')
+    }
+}
+
+export function interestValidation(request) {
+
+    if (isNaN(request['interest']) || isNaN(request['amount']) || isNaN(request['rate']) || isNaN('capital') || isNaN('duration')) {
+        throw Error('Valor inválido para pelo menos um dos campos')
+    }
+
+    let interest = request["interest"];
+    let capital = request["capital"];
+    let amount = request["amount"];
+    let rate = request["rate"];
+    let duration = request["duration"];
+    let isSimpleInterest = request["is-simple"]
+    let message = "Impossível calcular: ";
+
+    if (interest == null && capital == null && isSimpleInterest) {
+        message += 'Você deve preencher o campo de juros ou de capital.';
+    } else if (capital == null && amount == null && !isSimpleInterest) {
+        message += 'Capital e Montante não podem ser nulos ao mesmo tempo no cálculo de juros compostos.';
+    } else if (rate == null && interest == null && amount == null && isSimpleInterest) {
+        message += 'A taxa não pode ser nula juntamente com juros e montante.';
+    } else if (rate == null && capital == null && amount == null) {
+        message += 'A taxa não pode ser nula juntamente com capital e montante.';
+    } else if (duration == null && interest == null && amount == null && isSimpleInterest) {
+        message += 'A duração não pode ser nula juntamente com juros e montante em um cálculo de juros simples.';
+    } else if (rate == null && duration == null) {
+        message += 'A taxa não pode ser nula juntamente com a duração.';
+    }
+
+    if (message.length > 21) {
+        throw Error(message);
     }
 }
